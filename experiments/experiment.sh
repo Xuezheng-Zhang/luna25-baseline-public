a4710faee2e06708b322a4d846bc15a8fa8ab9cb
Samuel Padron Alcala <spadron05@gmail.com>
2025-04-25 12:55:38 +0200

    Update to run train script on slurm

--- experiments/experiment.sh
+++ experiments/experiment.sh
@@ -1,13 +1,15 @@
-BATCH --partition=csedu
+#!/usr/bin/env bash
+#SBATCH --partition=csedu
 #SBATCH --account=cseduproject # or your course code
 #SBATCH --gres=gpu:1
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=1
-#SBATCH --time=6:00:00
-#SBATCH --output=my-experiment-%j.out
-#SBATCH --error=my-experiment-%j.err
+#SBATCH --time=1:00:00
+#SBATCH --output=./logs/experiment1_%j.out
+#SBATCH --error=./logs/experiment1_%j.err
 #SBATCH --mail-user=YOURNAME
-#SBATCH --mail-type=BEGIN,END,FAIL
 
-echo "Hello! This very simple experiment works."
+project_dir=.
+source "$project_dir"/venv/bin/activate
+python3 "$project_dir"/train.py
 
